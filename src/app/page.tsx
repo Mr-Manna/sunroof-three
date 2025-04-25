42251c235b72e2bc2b01d9d2dc6f924ee4598a0b
chandan.hattimateam <chandan.hattimateam@gmail.com>
2025-04-25 17:49:33 +0530

    three js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,118 @@
+'use client'
 import Image from "next/image";
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState,useEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+import { PerspectiveCamera, PositionalAudio } from '@react-three/drei'
+
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef()
+  useFrame((state, delta) => {
+    meshRef.current.rotation.z += delta
+    meshRef.current.rotation.y += delta
+
+  })
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <PerspectiveCamera position={[0,0,0]}>
+
+    <mesh
+      {...props}
+        ref={meshRef}
+        scale={.1}
+        wireframe={true} 
+      >
+      <boxGeometry args={[props.boxWidth,props.boxHeight, props.boxLength]}/>
+      <meshStandardMaterial color={'#e59a1a'} height={props.height} width={props.width}/>
+    </mesh>
+    </PerspectiveCamera>
+
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [height,setHeight] = useState(100)
+  const [width,setWidth] = useState(100)
+  const [canvasBackground,setCanvasBackground] = useState('black')
+  const [boxWidth,setBoxWidth] = useState(10)
+  const [boxHeight,setBoxHeight] = useState(10)
+  const [boxLength,setBoxLength] = useState(10)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(()=>{
+    if (typeof window !== "undefined" ){
+      setHeight(window.innerHeight)
+      setWidth(window.innerWidth)
+      window.addEventListener('resize',()=>{
+        setHeight(window.innerHeight)
+        setWidth(window.innerWidth)
+      })
+    } 
+
+  })
+
+  const handleBoxHeightChange = (e)=>{
+    if(boxHeight === 10) return
+    setBoxHeight(e.target.value)
+  }
+
+  const handleBoxWidthChange = (e)=>{
+    if(boxWidth === 10) return
+    setBoxWidth(e.target.value)
+  }
+
+  const handleBoxLengthChange = (e)=>{
+    if(boxLength === 10) return
+    setBoxLength(e.target.value)
+  }
+
+  const increaseBoxHeight = (e)=>{
+    boxHeight === 15 ? null : setBoxHeight(boxHeight => boxHeight += 1)
+  }
+  const decreaseBoxHeight = (e)=>{
+    boxHeight === 10 ? null : setBoxHeight(boxHeight => boxHeight -= 1)
+  }
+
+  const increaseBoxWidth = (e)=>{
+    boxWidth === 15 ? null : setBoxWidth(boxWidth => boxWidth += 1)
+  }
+  const decreaseBoxWidth = (e)=>{
+    boxWidth === 10 ? null : setBoxWidth(boxWidth => boxWidth -= 1)
+  }
+
+  const increaseBoxLength = (e)=>{
+    boxLength === 15 ? null : setBoxLength(boxLength => boxLength += 1)
+  }
+  const decreaseBoxLength = (e)=>{
+    boxLength === 10 ? null : setBoxLength(boxLength => boxLength -= 1)
+  }
+  
+  return (
+    <div className="grid grid-cols-[60%_40%] ">
+      <div className="min-h-screen">
+      <Canvas className="bg-black">
+        <ambientLight intensity={1} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI/4} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI/2} />
+        <Box position={[0, 0, 0]} boxHeight={boxHeight} boxWidth={boxWidth} boxLength={boxLength}/>
+      </Canvas>
+      </div>
+      <div className="p-10">
+        <div className="flex flex-col">
+        <span className="text-5xl font-bold">Design. Customize.<br/>Experience.</span>
+        <div className="flex justify-around w-full my-5">
+          <div className="flex items-center width-[20%] p-[5px]"><span className="p-[1px]">Length : {boxLength} </span><input type="number" min="10" max="15" value={boxLength} onChange={handleBoxLengthChange}/> <span onClick={decreaseBoxLength} className="p-1 bg-stone-400 cursor-pointer">-</span> <span onClick={increaseBoxLength} className="p-1 bg-stone-400 cursor-pointer">+</span></div>
+        
+          <div className="flex items-center width-[20%] p-[5px]"><span className="p-[1px]">Width : {boxWidth} </span><input type="number" min="10" max="15" value={boxWidth} onChange={handleBoxWidthChange}/> <span onClick={decreaseBoxWidth} className="p-1 bg-stone-400 cursor-pointer">-</span> <span onClick={increaseBoxWidth} className="p-1 bg-stone-400 cursor-pointer">+</span></div>
+        
+          <div className="flex items-center width-[20%] p-[5px]"><span className="p-[1px]">Height : {boxHeight} </span><input type="number" min="10" max="15" value={boxHeight} onChange={handleBoxHeightChange}/> <span className="p-[5px] bg-stone-400 cursor-pointer" onClick={decreaseBoxHeight}>-</span> <span className="p-1 bg-stone-400 cursor-pointer" onClick={increaseBoxHeight}>+</span></div>
+        
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        </div>
+      </div>
     </div>
   );
 }
